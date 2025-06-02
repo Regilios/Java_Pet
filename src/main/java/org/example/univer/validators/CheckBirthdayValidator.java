0249fc522981928c9fb1a91cf724ff89e2afe446
Regilios <ahiro2011@gmail.com>
2025-06-02 09:49:39 +0500

    - fix

--- src/main/java/org/example/univer/validators/CheckBirthdayValidator.java
+++ src/main/java/org/example/univer/validators/CheckBirthdayValidator.java
@@ -7,22 +7,23 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Objects;
 
 @Component
 public class CheckBirthdayValidator implements ConstraintValidator<CheckBirthday, LocalDate> {
 
     @Override
-    public boolean isValid(LocalDate localDate, ConstraintValidatorContext context) {
-        if (localDate == null) {
+    public boolean isValid(LocalDate birthday, ConstraintValidatorContext context) {
+        if (Objects.isNull(birthday)) {
             return true;
         }
 
         LocalDate currentDate = LocalDate.now();
-        if (localDate.isAfter(currentDate)) {
+        if (birthday.isAfter(currentDate)) {
             return false;
         }
 
-        boolean isValid = Period.between(localDate, currentDate).getYears() >= 18;
+        boolean isValid = Period.between(birthday, currentDate).getYears() >= 18;
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
